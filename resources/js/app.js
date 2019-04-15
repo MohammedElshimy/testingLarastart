ad40617b6dccf0d3ec187d5c0fbcc73ba8b1a16a
MohammedElshimy <Mohammedelshimy@outlook.sa>
2019-04-15 12:00:35 +0200

    My very first commit

---
+++ resources/js/app.js
@@ -0,0 +1,90 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import moment from 'moment';
+import {Form, HasError, AlertError} from 'vform';
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+const toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+window.toast = toast;
+
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+})
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+import Dashboard from './components/Dashboard.vue';
+import Profile from './components/Profile.vue';
+import Users from './components/Users.vue';
+
+
+let routes = [
+    { path: '/dashboard', component: Dashboard},
+    { path: '/profile', component: Profile },
+    { path: '/Users', component: Users }
+];
+
+const router = new VueRouter({
+    hashbang: false,
+    history: true,
+    mode: 'history',
+    routes
+});
+
+
+Vue.filter('upText', function (text) {
+  return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate', function (created) {
+  return moment(created).format('MMMM Do YYYY, h:mm:ss a');
+})
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div id="app">
+            Form: <input type="text" v-model="orange">
+            To: <input type="text" v-model="destination">
+            <iframe width="100%" height="500px" src=""></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+        export default {
+             el:'#app',
+            data:{
+                orange:'Giza',
+                destination:'egypt',
+                url:"VUE_APP_GOOGLE_MAPS_API_KEY=ABcdEfGhIjklmNOpqrsTUvWXyzAbcD1EfGhiJKl"
+            },
+            methods:{
+                src:function(){
+                    return this.url="&orange"+orange+"&destination="+this.destination;
+                }
+            },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+    
+</script>
